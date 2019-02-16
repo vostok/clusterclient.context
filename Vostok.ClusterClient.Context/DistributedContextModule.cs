ba5656c20c2d8f91ba25c9855cd8fa43eff8da87
iloktionov <iloktionov@skbkontur.ru>
2019-02-16 21:50:22 +0500

    Fixed build.

--- Vostok.ClusterClient.Context/DistributedContextModule.cs
+++ Vostok.ClusterClient.Context/DistributedContextModule.cs
@@ -10,8 +10,8 @@ namespace Vostok.Clusterclient.Context
     /// <summary>
     /// <para>A module that does three things:</para>
     /// <list type="bullet">
-    ///     <item><description>Adds a <see cref="HeaderNames.VostokContextGlobals"/> header to request if there are any distributed <see cref="FlowingContext.Globals"/> in ambient context.</description></item>
-    ///     <item><description>Adds a <see cref="HeaderNames.VostokContextProperties"/> header to request if there are any distributed <see cref="FlowingContext.Properties"/> in ambient context.</description></item>
+    ///     <item><description>Adds a <see cref="HeaderNames.ContextGlobals"/> header to request if there are any distributed <see cref="FlowingContext.Globals"/> in ambient context.</description></item>
+    ///     <item><description>Adds a <see cref="HeaderNames.ContextProperties"/> header to request if there are any distributed <see cref="FlowingContext.Properties"/> in ambient context.</description></item>
     ///     <item><description>Sets the <see cref="RequestPriority"/> in request parameters if none was specified by user and there's currently a non-null nullable <see cref="RequestPriority"/> value in ambient context <see cref="FlowingContext.Globals"/>.</description></item>
     /// </list>
     /// </summary>
@@ -33,10 +33,10 @@ namespace Vostok.Clusterclient.Context
             var properties = FlowingContext.SerializeDistributedProperties();
 
             if (globals != null)
-                context.Request = context.Request.WithHeader(HeaderNames.VostokContextGlobals, globals);
+                context.Request = context.Request.WithHeader(HeaderNames.ContextGlobals, globals);
 
             if (properties != null)
-                context.Request = context.Request.WithHeader(HeaderNames.VostokContextProperties, properties);
+                context.Request = context.Request.WithHeader(HeaderNames.ContextProperties, properties);
         }
 
         private static void SetRequestPriority(IRequestContext context)
